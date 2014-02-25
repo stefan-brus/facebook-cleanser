54e00a8a037f40e412ee9ac6637f9e07581b9bd8
Stefan Brus <stefan1brus@gmail.com>
2014-02-25 01:25:01 +0100

    Add initial functionality
    Adds a functional userscript that removes the birthday notification.

---
+++ src/facebook-cleanser.js
@@ -0,0 +1,12 @@
+// ==UserScript==
+// @name       Facebook Cleanser
+// @namespace  http://github.com/stefan-brus/facebook-cleanser
+// @version    0.1
+// @description  Removes unwanted elements from facebook
+// @match      https://www.facebook.com/
+// @copyright  2014, Stefan Brus
+//
+// @require    http://ajax.googleapis.com/ajax/libs/jquery/1.6.4/jquery.min.js
+// ==/UserScript==
+
+$('#birthday_reminders_link').hide();
