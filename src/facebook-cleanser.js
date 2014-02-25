74369c60284a324ef371c3855b202d0ab71ab74a
Stefan Brus <stefan1brus@gmail.com>
2014-02-25 22:41:26 +0100

    Hide clutter from home page
    Addresses #2.
    Hides the clutter from the main facebook page.

--- src/facebook-cleanser.js
+++ src/facebook-cleanser.js
@@ -9,4 +9,32 @@
 // @require    http://ajax.googleapis.com/ajax/libs/jquery/1.6.4/jquery.min.js
 // ==/UserScript==
 
+// Hide 'favorites'
+$('#pinnedNav').hide();
+
+// Hide 'groups'
+$('#groupsNav').hide();
+
+// Hide 'apps'
+$('#appsNav').hide();
+
+// Hide 'friends'
+$('#listsNav').hide();
+
+// Hide 'pages'
+$('#pagesNav').hide();
+
+// Hide upper left profile link
+$('#pagelet_welcome_box').hide();
+
+// Hide birthdays
 $('#birthday_reminders_link').hide();
+
+// Hide events
+$('#event_reminders_link').hide();
+
+// Hide copyright footer
+$('#pagelet_rhc_footer').hide();
+
+// Hide recommended pages
+$('#pagelet_ego_pane').hide();
